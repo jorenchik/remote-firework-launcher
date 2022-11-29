e7d9ba7f79bd8edaa758e10bc47520f04a0b9298
jorenchik <jorens.stekels@gmail.com>
2022-11-29 08:30:45 +0200

    Added to comments to classes

--- esp-32-program/src/classes.h
+++ esp-32-program/src/classes.h
@@ -5,15 +5,15 @@ class Device
 {
   int status;
   int wifiMode;
-  std::vector<Pin*> pins;
-  std::vector<int> availablePinNumbers{13,16,17,18,19,21,22,23,25,26,27,32,33};
+  std::vector<Pin*> pins; // Active/inactive pins on the device
+  std::vector<int> availablePinNumbers{13,16,17,18,19,21,22,23,25,26,27,32,33}; // Pin numbers that will be initialized along the device
 
   public:
   Device(int mode);
   ~Device();
   void connectToWifi();
   int getStatus();
-  void initPins();
+  void initPins(); // Creates pin object for every available pinNumber
   std::vector<Pin*> *getPins();
   std::vector<int> *getAvailablePinNumbers();
 };
@@ -28,9 +28,9 @@ class Pin
   public:
   Pin(int pinNum, Device *device);
   ~Pin();
-  void enable();
-  void disable();
-  void fire();
+  void enable(); // Makes the pin available for it to be fired
+  void disable(); // Makes the pin unavailable for it to be fired
+  void fire(); 
   int getStatus();
   int getPinNumber();
   Device *getDevice();
