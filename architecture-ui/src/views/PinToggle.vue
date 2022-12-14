012aa4a5ee97571ef181ea89b168e94ac67396e1
jorenchik <jorens.stekels@gmail.com>
2022-12-14 09:01:35 +0200

    Toggle button container styling

--- architecture-ui/src/views/PinToggle.vue
+++ architecture-ui/src/views/PinToggle.vue
@@ -53,61 +53,60 @@ form {
   display: flex;
   flex-direction: column;
   align-items: center;
-
 }
-
 label {
-
   font-weight: bold;
 }
-
-
 .pin-selection-container {
   flex-direction: column;
   align-items: center;
-  display:inline-flex;
+  display: flex;
   background: #8F6593;
-  width: 150px;
+  width: 60%;
   padding: 20px;
   border-radius: 20px;
   margin-bottom: 10px;
 }
 
+@media (min-width: 768px) {
+  .pin-selection-container {
+    width: 20rem;
+  }
+}
+
 .pin-selection-item {
-  padding: 3px 10px;
-  margin: 3px auto;
+  padding-top: .5rem;
+  padding-bottom: .5rem;
+  text-transform: uppercase;
+  display: flex;
+  justify-content: center;
+  place-content: center;
   border-radius: 5px;
   font-size: 14px;
   margin-bottom: 10px;
   background-color: #8F6593;
   width: 90%;
 }
-
 .button {
-background-color: #008577;
-border-radius: 7px;
-padding: 10px 15px;
-margin: 5px auto;
-color: #23001E;
-font-weight: bold;
-display: flex;
+  background-color: #008577;
+  border-radius: 7px;
+  padding: 10px 15px;
+  margin: 5px auto;
+  color: #23001E;
+  font-weight: bold;
+  display: flex;
 }
-
-.checkbox {
-/* -webkit-appearance: none; */
-
-border: 1px solid #000;
-width: 15px;
-height: 15px;
-border-radius: 2px;
-margin-right: 10px;
-cursor: pointer;
-
+.checkbox, .radio {
+  border: 1px solid #000;
+  width: 15px;
+  height: 15px;
+  border-radius: 2px;
+  margin: 0 1rem 0 0;
+  cursor: pointer;
 }
 .enabled{
   background: #42b983;
   color: black;
-
 }
 .disabled{
   background: #7353ba;
