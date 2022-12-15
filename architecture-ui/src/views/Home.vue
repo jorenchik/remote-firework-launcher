aad4b1b5d4d83dda0eb323360ba23e62f8ec8104
jorenchik <jorens.stekels@gmail.com>
2022-12-15 11:42:47 +0200

    Changed message on the home page

--- architecture-ui/src/views/Home.vue
+++ architecture-ui/src/views/Home.vue
@@ -2,15 +2,12 @@
   <div class="home">
     <div>
       <h1>Home</h1>
-      <p>Welcome to the ESP8266 pin configuration page.</p>
+      <p>Welcome to the ESP32 remote control app!</p>
     </div>
   </div>
 </template>
 
-
-
 <script>
-
 export default {
   name: 'Home',
 }
