f8e16f67cb5503a9da2cea0d2393548554e5a79c
jorenchik <jorens.stekels@gmail.com>
2022-12-16 20:01:09 +0200

    Responsive form width

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-   <NavBar/> 
+  <NavBar />
 
-   <router-view/>
+  <router-view />
 
 </template>
 
@@ -9,115 +9,144 @@
 import NavBar from './components/NavBar.vue'
 export default {
   name: 'Home',
-  components: { NavBar},
+  components: { NavBar },
 
 }
 
 </script>
 
-<style> 
-  /* Document styles */
-  body {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #7353ba;
-    background: #23001E; 
-  }
-  .component-container {
-    display: flex;
-    width: 100%;
-    place-content: center;
-  }
-  /* Navbar */
-  nav {
-    padding: 30px;
-  }
-  nav a {
-    font-weight: bold;
-    color: #7353ba;
-  }
-  nav a.router-link-exact-active {
-    color: #42b983;
-  }
-  
-  /* Form */
-  form {
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    align-items: center;
-  }
-  .form-container {
-    display: flex;
-    place-items: center;
-    place-content: center;
-    width: 80%;
-  }
-  label {
-    font-weight: bold;
-  }
-  .pin-selection-container {
-    flex-direction: column;
-    align-items: center;
-    display: flex;
-    background: #8F6593;
-    padding: 20px;
-    border-radius: 20px;
-    margin-bottom: 10px;
-  }
-  .pin-selection-item {
-    padding-top: .5rem;
-    padding-bottom: .5rem;
-    text-transform: uppercase;
-    display: flex;
-    justify-content: center;
-    place-content: center;
-    border-radius: 5px;
-    font-size: 14px;
-    margin-bottom: 10px;
-    background-color: #8F6593;
-    width: 90%;
-  }
-  .text-input {
-    margin-bottom: 0.5rem;
-  }
-  .button {
-    background-color: #008577;
-    border-radius: 7px;
-    padding: 10px 15px;
-    margin: 5px auto;
-    color: #23001E;
-    font-weight: bold;
-    display: flex;
-  }
-  .checkbox, .radio {
-    border: 1px solid #000;
-    width: 15px;
-    height: 15px;
-    border-radius: 2px;
-    margin: 0 1rem 0 0;
-    cursor: pointer;
-  }
-  .error{
-      width: 400px;
-  }
-  /* Buttons */
-  .neutral {
-    background: #4f8f72;
-    color: black;
-  }
-  .enabled{
-    background: #42b983;
-    color: black;
-  }
-  .disabled{
-    background: #7353ba;
-    color: white;
-  }
-  .fired{
-    background: #23001E;
-    color: white;
-  }
+<style>
+/* Document styles */
+ body {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #7353ba;
+   background: #23001E;
+ }
+
+ .component-container {
+   display: flex;
+   width: 100%;
+   place-content: center;
+ }
+
+ /* Navbar */
+ nav {
+   padding: 30px;
+ }
+
+ nav a {
+   font-weight: bold;
+   color: #7353ba;
+ }
+
+ nav a.router-link-exact-active {
+   color: #42b983;
+ }
+
+ /* Form */
+ form {
+   display: flex;
+   width: 100%;
+   flex-direction: column;
+   align-items: center;
+ }
+
+ .form-container {
+   display: flex;
+   place-items: center;
+   place-content: center;
+   width: 70%;
+ }
+
+ @media (min-width: 768px) {
+   .form-container {
+     width: 30%;
+   }
+ }
+
+ @media (min-width: 1280px) {
+   .form-container {
+     width: 20%;
+   }
+ }
+
+ label {
+   font-weight: bold;
+ }
+
+ .pin-selection-container {
+   flex-direction: column;
+   align-items: center;
+   display: flex;
+   background: #8F6593;
+   padding: 20px;
+   border-radius: 20px;
+   margin-bottom: 10px;
+ }
+
+ .pin-selection-item {
+   padding-top: .5rem;
+   padding-bottom: .5rem;
+   text-transform: uppercase;
+   display: flex;
+   justify-content: center;
+   place-content: center;
+   border-radius: 5px;
+   font-size: 14px;
+   margin-bottom: 10px;
+   background-color: #8F6593;
+   width: 90%;
+ }
+
+ .text-input {
+   margin-bottom: 0.5rem;
+ }
+
+ .button {
+   background-color: #008577;
+   border-radius: 7px;
+   padding: 10px 15px;
+   margin: 5px auto;
+   color: #23001E;
+   font-weight: bold;
+   display: flex;
+ }
+
+ .checkbox,
+ .radio {
+   border: 1px solid #000;
+   width: 15px;
+   height: 15px;
+   border-radius: 2px;
+   margin: 0 1rem 0 0;
+   cursor: pointer;
+ }
+
+ .error {
+   width: 400px;
+ }
+
+ /* Buttons */
+ .neutral {
+   background: #4f8f72;
+   color: black;
+ }
+
+ .enabled {
+   background: #42b983;
+   color: black;
+ }
+
+ .disabled {
+   background: #7353ba;
+   color: white;
+ }
+
+ .fired {
+   background: #23001E;
+   color: white;
+ }
 </style>
